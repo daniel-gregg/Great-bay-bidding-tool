05e1ff818ee7f827995231e2025b219665bfb44f
Spaniel Boone <spaniel.boone@gmail.com>
2021-05-18 21:10:38 +0930

    added a conceptual outline for inquirer interactions

---
+++ questions.js
@@ -0,0 +1,13 @@
+//what is your login name?
+    //if exists then next
+    //if not add to users
+
+//do you want to post an item or search items
+
+//if search items:
+    //then send back all items as list in inquirer
+    //if users choose item
+        //then present current lead bid in console AND ask for a bid or not
+
+//if post an item:
+    //then ask item details
